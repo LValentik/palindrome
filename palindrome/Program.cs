d639be3ca7df2c4e0902d76489b193586c4c13f9
Valentík Lukáš <Valentik.Lu.2022@skola.ssps.cz>
2022-11-08 10:48:10 +0100

    first version

---
+++ palindrome/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string c = x.ToString();
+            int x2 = x;
+            int s = 0;
+            if (x < 0)
+            {
+                Console.WriteLine("Vaše číslo není palindrom");
+            }
+            else
+            {
+                for (int i = 0; i < c.Length; i++)
+                {
+                    s = (s * 10) + (x % 10);
+                    x = x / 10;
+                    Console.WriteLine(s);
+                }
+                if (s == x2)
+                {
+                    Console.WriteLine("Vaše číslo je palindrom");
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Vaše číslo není palindrom");
+                }
+
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
