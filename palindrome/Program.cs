dfde165e07101c88eed6dfd087f0956928e2b386
Valentík Lukáš <Valentik.Lu.2022@skola.ssps.cz>
2022-11-08 11:12:24 +0100

    actual v2

--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -6,35 +6,46 @@ namespace palindrome
     {
         static void Main(string[] args)
         {
-            int x = int.Parse(Console.ReadLine());
-            string c = x.ToString();
-            int x2 = x;
-            int s = 0;
-            if (x < 0)
-            {
-                Console.WriteLine("Vaše číslo není palindrom");
-            }
-            else
-            {
-                for (int i = 0; i < c.Length; i++)
-                {
-                    s = (s * 10) + (x % 10);
-                    x = x / 10;
-                    Console.WriteLine(s);
-                }
-                if (s == x2)
-                {
-                    Console.WriteLine("Vaše číslo je palindrom");
+          
+            Console.WriteLine("Chcete spustit program?");
+            string start = Console.ReadLine;      
 
-                }
+            while (start == "ano")
+            {
 
-                else
+                int x = int.Parse(Console.ReadLine());
+                string c = x.ToString();
+                int x2 = x;
+                int s = 0;
+                if (x < 0)
                 {
                     Console.WriteLine("Vaše číslo není palindrom");
                 }
+                else
+                {
+                    for (int i = 0; i < c.Length; i++)
+                    {
+                        s = (s * 10) + (x % 10);
+                        x = x / 10;
+                        Console.WriteLine(s);
+                    }
+                    if (s == x2)
+                    {
+                        Console.WriteLine("Vaše číslo je palindrom");
 
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Vaše číslo není palindrom");
+                    }
+
+                }
+                Console.WriteLine("Chcete program znovu spustit?");
+                start = Console.ReadLine("");
             }
-            Console.ReadLine();
+
+            Console.ReadKey();
 
 
 
